d16a56a1a3d10d97bdbb6ed98de4d7c915daec44
João Pedro Pedroso <jpp@fc.up.pt>
2021-07-22 09:08:03 +0100

    Read and write to Firestore, securely!

--- streamlit_rw.py
+++ streamlit_rw.py
@@ -1,41 +1,45 @@
 import streamlit as st
 from google.cloud import firestore
-import json
-key_dict = json.loads(st.secrets["textkey"])
-# creds = service_account.Credentials.from_service_account_info(key_dict)
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("users").document("jpp9374")
+
+# This time, we're creating a NEW post reference for Apple
+doc_ref = db.collection("users").document("jpp0123")
+
+# And then uploading some data to that reference
+doc_ref.set({
+    "title": "JPP title",
+    "url": "www.joaopedroso.org"
+})
 
 
-from google.oauth2 import service_account
-creds = service_account.Credentials.from_service_account_info(key_dict)
 
 
-
-
-db = firestore.Client(credentials=creds, project="streamlit-98234")
 
 # Streamlit widgets to let a user create a new post
 title = st.text_input("Post title")
 url = st.text_input("Post url")
-email = st.text_input("Post e-mail")
 submit = st.button("Submit new post")
 
 # Once the user has submitted, upload it to the database
 if title and url and submit:
-    doc_ref = db.collection("onln").document(title)
+    doc_ref = db.collection("users").document(title)
     doc_ref.set({
         "title": title,
-        "url": url,
-        "email": email,
+        "url": url
     })
 
 # And then render each post, using some light Markdown
-users_ref = db.collection("onln")
+users_ref = db.collection("users")
 for doc in users_ref.stream():
     post = doc.to_dict()
     try:
         title = post["title"]
         url = post["url"]
-        email = post["email"]
 
         st.subheader(f"Post: {title}")
         st.write(f":link: [{url}]({url})")
@@ -44,7 +48,7 @@ for doc in users_ref.stream():
         st.write(f"[post with no title:] {post}")
 
 # Now let's make a reference to ALL of the users
-users_ref = db.collection("onln")
+users_ref = db.collection("users")
 
 # For a reference to a collection, we use .stream() instead of .get()
 for doc in users_ref.stream():
